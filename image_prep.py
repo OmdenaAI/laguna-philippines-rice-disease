28866b06956ec9c4de7971a4596d18278e202e1e
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2023-04-24 22:53:20 +0800

    Add files via upload

--- image_prep.py
+++ image_prep.py
@@ -1,53 +1,75 @@
 import cv2
 import numpy as np
 import os
+from tkinter import filedialog
+from tkinter import *
 
-# Specify the input directory containing leaf images
-input_dir = 'C:/Users/Rocelle Nathalie Ong/Downloads/initial/fungal_diseases/input'
 
-# Specify the output directory to save processed images
-output_dir = 'C:/Users/Rocelle Nathalie Ong/Downloads/initial/fungal_diseases/processed_images/'
+def preprocess_images(input_dir, output_dir):
+    for filename in os.listdir(input_dir):
+        if filename.endswith('.jpg') or filename.endswith('.jpeg') or filename.endswith('.png'):
+            img = cv2.imread(os.path.join(input_dir, filename))
+            img = cv2.resize(img, (224, 224))
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            blur = cv2.GaussianBlur(gray, (5, 5), 0)
+            ret, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            areas = [cv2.contourArea(c) for c in contours]
+            max_index = np.argmax(areas)
+            max_contour = contours[max_index]
+            mask = np.zeros(img.shape[:2], np.uint8)
+            cv2.drawContours(mask, [max_contour], 0, 255, -1)
+            masked_img = cv2.bitwise_and(img, img, mask=mask)
+            output_filename = os.path.join(output_dir, filename)
+            cv2.imwrite(output_filename, masked_img)
 
-# Create the output directory if it does not exist
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
 
-# Loop through each image file in the input directory
-for filename in os.listdir(input_dir):
-    if filename.endswith('.jpg') or filename.endswith('.jpeg') or filename.endswith('.png'):
-        
-        # Load the input image
-        img = cv2.imread(os.path.join(input_dir, filename))
-        
-        # Resize the image to a specific size (e.g., 224x224)
-        img = cv2.resize(img, (224, 224))
+def browse_input_folder():
+    global input_folder_path
+    input_folder_path = filedialog.askdirectory()
+    input_folder_label.config(text=input_folder_path)
 
-        # Convert the image to grayscale
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Apply Gaussian blur to remove noise
-        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+def browse_output_folder():
+    global output_folder_path
+    output_folder_path = filedialog.askdirectory()
+    output_folder_label.config(text=output_folder_path)
 
-        # Apply Otsu's thresholding to segment the leaf from the background
-        ret, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-        # Find contours in the binary image
-        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+def start_preprocessing():
+    if input_folder_path and output_folder_path:
+        preprocess_images(input_folder_path, output_folder_path)
+        status_label.config(text="Image preprocessing completed!")
+    else:
+        status_label.config(text="Please select both input and output folders.")
 
-        # Find the largest contour (leaf) in the image
-        areas = [cv2.contourArea(c) for c in contours]
-        max_index = np.argmax(areas)
-        max_contour = contours[max_index]
 
-        # Create a mask of the same size as the input image
-        mask = np.zeros(img.shape[:2], np.uint8)
+# Create the main Tkinter window
+root = Tk()
+root.title("Image Preprocessing")
+root.geometry("500x200")
+root.resizable(False, False)
 
-        # Draw the contour on the mask
-        cv2.drawContours(mask, [max_contour], 0, 255, -1)
+input_folder_path = ""
+output_folder_path = ""
 
-        # Apply bitwise AND operation to remove the background
-        masked_img = cv2.bitwise_and(img, img, mask=mask)
+# Create and position the input folder button and label
+input_folder_button = Button(root, text="Browse Input Folder", command=browse_input_folder)
+input_folder_button.grid(row=0, column=0, padx=10, pady=10)
+input_folder_label = Label(root, text="")
+input_folder_label.grid(row=0, column=1, padx=10, pady=10)
 
-        # Save the processed image in the output directory
-        output_filename = os.path.join(output_dir, filename)
-        cv2.imwrite(output_filename, masked_img)
+# Create and position the output folder button and label
+output_folder_button = Button(root, text="Browse Output Folder", command=browse_output_folder)
+output_folder_button.grid(row=1, column=0, padx=10, pady=10)
+output_folder_label = Label(root, text="")
+output_folder_label.grid(row=1, column=1, padx=10, pady=10)
+
+# Create and position the start button and status label
+start_button = Button(root, text="Start Preprocessing", command=start_preprocessing)
+start_button.grid(row=2, column=0, padx=10, pady=10)
+status_label = Label(root, text="")
+status_label.grid(row=2, column=1, padx=10, pady=10)
+
+# Start the Tkinter main loop
+root.mainloop()
